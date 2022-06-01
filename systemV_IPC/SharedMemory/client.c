22c6802f496c0f85e776bc659ece803c09e3a8e4
itto3o <208ehd@naver.com>
2022-06-01 14:56:34 +0900

    when use a shared memory, memcpy than strcpy

--- systemV_IPC/SharedMemory/client.c
+++ systemV_IPC/SharedMemory/client.c
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/mman.h>
 
 #define BUF_SIZE 1024
 
@@ -12,32 +13,48 @@ int main(int argc, char* argv[]) {
 	key_t key;
 	int shmid;
 	void* shmaddr;
-	char buf[BUF_SIZE];
-	int flag = 0;
 
+	//flag와 buf를 malloc으로 동적할당
+	char* buf = (char*)malloc(sizeof(char) * BUF_SIZE);
+	int* flag = (int*)malloc(sizeof(int));
+	//client의 초기 flag값은 0
+	*flag = 0;
+
+	//server와 동일한 shmfile에서 key 생성 후 shm 생성
 	key = ftok("shmfile", 1);
-	shmid = shmget(key, BUF_SIZE, IPC_CREAT|0666);
+	shmid = shmget(key, BUF_SIZE, 0);
 	if(shmid == -1) {
 		perror("shmget error");
 		exit(1);
 	}
 
+	//shm을 사용할 수 있도록 attach
 	shmaddr = shmat(shmid, NULL, 0);
+	memset(shmaddr, 0, sizeof(shmaddr));
 
 	while(1) {
-		flag = *((int*)shmaddr);
-		printf("%d\n", flag);
-		if(flag == 1) {
-			strcpy(buf, shmaddr);
-			printf("Server Said :%s\n", buf);
+		//계속 shm의 flag값을 읽어오다가 1이 되면 수행
+		*flag = *((int*)shmaddr);
+		if((*flag) == 1) {
+			//1일 때 공유메모리의 내용을 읽어오고 출력
+			strcpy(buf, ((char*)shmaddr + sizeof(int*)));
+			printf("Server Said : %s\n", buf);
 
-			printf("Client >>");
+			//출력 후 입력
+			printf("Client >> ");
 			fgets(buf, BUF_SIZE, stdin);
-			flag = 0;
-			shmaddr = &flag;
-			strcpy(shmaddr + sizeof(int), buf);
+			
+			//flag를 0으로 바꾸고 memcpy로 shm에 write
+			*flag = 0;
+			memcpy(shmaddr, flag, sizeof(int*));
+			memcpy(((char*)shmaddr + sizeof(int*)), buf, sizeof(buf));
+			//shmaddr = (void*)flag;
+			//strcpy(((char*)shmaddr + sizeof(int*)), buf);
 		}
 	}
+	shmdt(shmaddr);
 	
+	free(flag);
+	free(buf);
 	return 0;
 }
--- systemV_IPC/SharedMemory/server.c
+++ systemV_IPC/SharedMemory/server.c
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/mman.h>
 
 #define BUF_SIZE 1024
 
@@ -12,9 +13,14 @@ int main(int argc, char* argv[]) {
 	key_t key;
 	int shmid;
 	void* shmaddr;
-	char buf[BUF_SIZE];
-	int flag = 1;
 
+	//flag와 내용을 malloc 동적 할당
+	int* flag = (int*)malloc(sizeof(int));
+	char* buf = (char*)malloc(sizeof(char) * BUF_SIZE);
+	//server의 초기 플래그 값은 1
+	*flag = 1;
+
+	//shmfile에서 key 생성, 공유메모리 생성
 	key = ftok("shmfile", 1);
 	shmid = shmget(key, BUF_SIZE, IPC_CREAT|0666);
 	if(shmid == -1) {
@@ -22,26 +28,43 @@ int main(int argc, char* argv[]) {
 		exit(1);
 	}
 
+	//shm을 사용할 수 있도록 attach
 	shmaddr = shmat(shmid, NULL, 0);
+	memset(shmaddr, 0, sizeof(shmaddr));
 
+	//server에서 입력 먼저 시작
 	printf("Server >> ");
 	fgets(buf, BUF_SIZE, stdin);
-	shmaddr = &flag;
-	strcpy(shmaddr + sizeof(int), buf);
+
+	//받아온 값을 memcpy로 shmaddr에 입력,
+	//첫 부분에는 flag를, flag다음에는 contents를 입력
+	memcpy(shmaddr, flag, sizeof(int*));
+	memcpy(((char*)shmaddr + sizeof(int*)), buf, sizeof(buf));
+	//shmaddr = (void*)flag;
 
 	while(1) {
-		flag = *((int*)shmaddr);
-		if(flag == 0) {
-			strcpy(buf, shmaddr);
-			printf("Client Said :%s", buf);
+		//shm의 flag를 계속 읽어오다가
+		*flag = *((int*)shmaddr);
+		//flag가 0일 때 수행
+		if((*flag) == 0) {
+			//buf에 shm에서 읽어온 내용을 넣고 출력
+			strcpy(buf, ((char*)shmaddr + sizeof(int*)));
+			printf("Client Said : %s\n", buf);
 
-			printf("Server >>");
+			//출력 후 입력하기
+			printf("Server >> ");
 			fgets(buf, BUF_SIZE, stdin);
-			flag = 1;
-			shmaddr = &flag;
-			strcpy(shmaddr + sizeof(int), buf);
+			//flag를 1로 바꾸고
+			*flag = 1;
+			//flag와 입력한 내용을 memcpy로 write
+			memcpy(shmaddr, flag, sizeof(int*));
+			memcpy(((char*)shmaddr + sizeof(int*)), buf, sizeof(buf));
 		}
 	}
+	shmdt(shmaddr);
+
+	free(flag);
+	free(buf);
 	
 	return 0;
 }
